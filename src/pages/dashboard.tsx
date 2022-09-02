3127f91f8d4d9fe83ff84375f4f5aabd1ab94dd4
Andre Grillo <andrell.grillo@gmail.com>
2022-09-02 10:04:17 -0300

    apexchart finish

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/sidebar";
 
-const Chart = dynamic(() => import('react-apexcharts'), {
+const ReactApexChart  = dynamic(() => import('react-apexcharts'), {
   ssr: false,
 })
 
@@ -47,12 +47,29 @@ const options = {
       '2021-03-23T00:00:00.000Z',
       '2021-03-24T00:00:00.000Z'
     ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3
+    }
   }
 };
 
 const series = [
   { name: 'series1', data: [31,120,18,26,62,18,150]}
 ];
+
+const options2 = {
+    labels: ['A','B','C','D','E','F','G'],
+    legend: {
+      show: false
+    },
+}
+const series2 = [31,120,18,26,62,18,150];
 
 export default function Dashboard() {
   return (
@@ -64,11 +81,11 @@ export default function Dashboard() {
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
           <Box p="8" bg="gray.800" borderRadius={8} pb="4" >
             <Text fontSize="lg" mb="4">Inscritos da Semana</Text>
-            <Chart options={options} series={series} type="area" height={160} />
+            <ReactApexChart options={options} series={series} type="area" height={175} />
           </Box>
           <Box p="8" bg="gray.800" borderRadius={8} pb="4" >
             <Text fontSize="lg" mb="4">Taxa de abertura</Text>
-            <Chart options={options} series={series} type="area" height={160} />
+            <ReactApexChart options={options2} series={series2} type="donut" height={210} />
           </Box>
         </SimpleGrid>
       </Flex>
